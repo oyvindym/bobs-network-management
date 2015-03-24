2c8cabe756e8aacaa1248286dd6622b82dbb0f07
oyvindym <moldestad@gmail.com>
2015-03-24 14:51:20 +0100

    Update views.py

--- bobs_network_management/app/controlpanel/views.py
+++ bobs_network_management/app/controlpanel/views.py
@@ -3,17 +3,24 @@ from bobs_network_management.app.snmp.snmp import SNMP
 from bobs_network_management.app.cim.cim import CimParser
 
 def controlpanel(request):
-	snmp_agents = getSNMPSystems()
-	cim = getCIMSystems()
+	"""
+	Method called when the user enters Bobs network management site.
+	"""
 	return render(request, 'controlpanel/base.html',
-		{"snmp_agents":snmp_agents,
-		"cim":cim})
+		{"snmp_agents":getSNMPSystems(),
+		"cim":getCIMSystems()})
 
 def getSNMPSystems():
+	"""
+	Returns all SNMP systems in the network
+	"""
 	snmp = SNMP("ttm4128", "2c")
 	snmp.work()
 	return snmp.getSystems()
 
 def getCIMSystems():
+	"""
+	Returns all CIM systems in the network
+	"""
 	cim = CimParser("http://ttm4128.item.ntnu.no:5988/root/cimv2")
 	return cim.getSystemInformation()
